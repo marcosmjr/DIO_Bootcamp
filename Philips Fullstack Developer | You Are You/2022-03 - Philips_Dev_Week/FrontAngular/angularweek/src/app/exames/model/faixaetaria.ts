b2e5357c14d2eabdae4d2b924dc187ad0b414758
marcosmjr <marcos.moreira.jr@gmail.com>
2022-03-16 17:00:25 -0300

    remoção de arquivos

--- Philips Fullstack Developer | You Are You/2022-03 - Philips_Dev_Week/FrontAngular/angularweek/src/app/exames/model/faixaetaria.ts
+++
@@ -1,6 +0,0 @@
-export interface Faixaetaria {
-    id: number,
-    faixa_i: number,
-    faixa_n: number,
-    descricao: string
-}
--- Philips Fullstack Developer | You Are You/2022-03 - Philips_Dev_Week/devweek/src/main/java/com/dio_class/devweek/repo/RegiaoRepo.java
+++
@@ -1,8 +0,0 @@
-package com.dio_class.devweek.repo;
-
-
-import com.dio_class.devweek.entity.Regiao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegiaoRepo extends JpaRepository<Regiao, Long> {
-}
